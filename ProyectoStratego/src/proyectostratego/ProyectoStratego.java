af172a454b6d94594163ba7e4f4a4cbe327d244e
JustPreo <impre0hn@gmail.com>
2025-05-30 08:13:40 -0600

    a

--- ProyectoStratego/src/proyectostratego/ProyectoStratego.java
+++ ProyectoStratego/src/proyectostratego/ProyectoStratego.java
@@ -5,6 +5,7 @@ public class ProyectoStratego {
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo!");
+        System.out.println("Huevos con aceite");
         
     }
     
